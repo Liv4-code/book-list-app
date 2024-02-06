57b98a4b793f1d53b6dd056d7c8ef84f2b1b9e35
livi <scholtz.ice@gmail.com>
2024-02-06 14:08:24 +0200

    Created book creation functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { useState } from "react";
-import "./index.css";
 import BookList from "./components/BookList";
 import BookCreate from "./components/BookCreate";
 
 const App = () => {
     const [books, setBooks] = useState([]);
 
+    const createBook = (title) => {
+        const updatedBooks = [
+            ...books,
+            { id: Math.round(Math.random * 9999), title },
+        ];
+
+        setBooks(updatedBooks);
+    };
+
+    const editBook = () => {};
+
+    const deleteBook = () => {};
+
     return (
         <div>
             <BookList />
-            <BookCreate />
+            <BookCreate onCreate={createBook} />
         </div>
     );
 };
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,17 +1,35 @@
-const BookEdit = ({ createBook }) => {
+import { useState } from "react";
+
+const BookCreate = ({ onCreate }) => {
+    const [title, setTitle] = useState("");
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        onCreate(title);
+        setTitle("");
     };
 
     return (
-        <div>
-            <h2>Add A Book</h2>
-            <label htmlFor="bookCreate">Title:</label>
-            <input type="text" name="bookCreate" />
-            <button onClick={handleSubmit} type="submit">
-                Submit
-            </button>
+        <div className="book-create">
+            <form onSubmit={handleSubmit}>
+                <h3>Add A Book</h3>
+                <label htmlFor="bookCreate">Title:</label>
+                <input
+                    className="input"
+                    onChange={handleChange}
+                    type="text"
+                    name="bookCreate"
+                    value={title}
+                />
+                <button className="button" onClick={handleSubmit} type="submit">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
-export default BookEdit;
+export default BookCreate;
