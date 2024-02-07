d02ea75a4891733a8b353e11ca000990143b93ea
livi <scholtz.ice@gmail.com>
2024-02-07 12:13:27 +0200

    Added random pic generator to book show

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -24,6 +24,10 @@ const BookShow = ({ book, onDelete, onEdit }) => {
 
     return (
         <div className="book-show">
+            <img
+                src={`https://picsum.photos/seed/${book.id}/300/200`}
+                alt="books"
+            />
             <div>{content}</div>
             <div className="actions">
                 <button className="edit" onClick={handleEditClick}>
