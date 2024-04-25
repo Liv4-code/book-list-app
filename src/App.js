a4edd63a427323f3d3f5217204fc8a19805a031a
livi <scholtz.ice@gmail.com>
2024-04-25 15:47:55 +0200

    Used axios to fetch and post books data to local json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import axios from "axios";
 import BookList from "./components/BookList";
 import BookCreate from "./components/BookCreate";
 
 const App = () => {
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        // Make get request to json server
+        const res = await axios.get("http://localhost:3001/books");
+        // Response gives back books that we now use set to books state
+        setBooks(res.data);
+    };
 
     const editBookById = (id, newTitle) => {
         const updatedBooks = books.map((book) => {
@@ -25,12 +33,14 @@ const App = () => {
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
-        const updatedBooks = [
-            ...books,
-            { id: Math.round(Math.random() * 9999), title },
-        ];
-
+    const createBook = async (title) => {
+        // Make post request to json server
+        const res = await axios.post("http://localhost:3001/books", {
+            title
+        })
+        // Receive response that gives back book with its id
+        const updatedBooks = [...books,res.data];
+        // Take response and add to book state
         setBooks(updatedBooks);
     };
 
