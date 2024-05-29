d962f90008997d1187b8346e94210eddb29d97bc
livi <scholtz.ice@gmail.com>
2024-05-29 10:31:10 +0200

    Refactored BookList to use cntxt

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,9 @@ import BookShow from "./BookShow";
 import { useContext } from "react";
 import BooksContext from "../context/books";
 
-const BookList = ({ books, onDelete, onEdit }) => {
+const BookList = () => {
+    const { books } = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return (
             <BookShow
